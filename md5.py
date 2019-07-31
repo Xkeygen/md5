1a208273ba2679573ed28404748bde8ad7460a49
Xkeygen <52706197+Xkeygen@users.noreply.github.com>
2019-08-01 08:42:01 +0900

    Add files via upload
    md5 decrypt python

---
+++ md5.py
@@ -0,0 +1,232 @@
+from os import system, name
+import time
+import hashlib
+def md5(str):
+    m = hashlib.md5()
+    m.update(str.encode())
+    return m.hexdigest()
+def cekmd5(str):
+    if md5(n)== '10880abdacdfe9efdc203e320d089159' or md5(n) =='6ea3974da9c6732c4b15d137ee95983f':
+        print('Password Found : ',n)
+        exit()
+    time.sleep(0)
+
+system('clear')
+x = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()_+=-,./;:" '
+print("--Sedang Proses Deskripsi--")
+for l1 in x:
+    n = l1
+    cekmd5(md5(n))
+    print('Cek :',l1,end="\r")
+for l1 in x:
+    for l2 in x:
+        n=l1+l2
+        cekmd5(md5(n))
+        print('cek :',l1+l2,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            n = l1 + l2 + l3
+            cekmd5(md5(n))
+            print('cek :', l1 + l2  + l3,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                n = l1 + l2+ l3  + l4
+                cekmd5(md5(n))
+                print('cek :', l1 + l2+ l3  + l4,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    n = l1 + l2+ l3+ l4  + l5
+                    cekmd5(md5(n))
+                    print('cek :', l1 + l2+ l3+ l4  + l5,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        n = l1 + l2+ l3+ l4  + l5 + l6
+                        cekmd5(md5(n))
+                        print('cek :', l1 + l2+ l3+ l4  + l5 + l6,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            n = l1 + l2 + l3 + l4 + l5 + l6+ l7
+                            cekmd5(md5(n))
+                            print('cek :',l1 + l2 + l3 + l4 + l5 + l6+ l7,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                n = l1 + l2 + l3 + l4 + l5 + l6+ l7+l8
+                                cekmd5(md5(n))
+                                print('cek :', l1 + l2 + l3 + l4 + l5 + l6+ l7+l8,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    n = l1 + l2 + l3 + l4 + l5 + l6+ l7+l8+l9
+                                    cekmd5(md5(n))
+                                    print('cek :', l1 + l2 + l3 + l4 + l5 + l6+ l7+l8+l9,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    for l10 in x:
+                                        n = l1 + l2 + l3 + l4 + l5 + l6+ l7+l8+l9+l10
+                                        cekmd5(md5(n))
+                                        print('cek :', l1 + l2 + l3 + l4 + l5 + l6+ l7+l8+l9+l10,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    for l10 in x:
+                                        for l11 in x:
+                                            n = l1 + l2 + l3 + l4 + l5 + l6+ l7+l8+l9+l10+l11
+                                            cekmd5(md5(n))
+                                            print('cek :', l1 + l2 + l3 + l4 + l5 + l6+ l7+l8+l9+l10+l11,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    for l10 in x:
+                                        for l11 in x:
+                                            for l12 in x:
+                                                n = l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11+l12
+                                                cekmd5(md5(n))
+                                                print('cek :', l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11+l12,
+                                                      end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    for l10 in x:
+                                        for l11 in x:
+                                            for l12 in x:
+                                                for l13 in x:
+                                                    n = l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11+ l12 + l13
+                                                    cekmd5(md5(n))
+                                                    print('cek :',
+                                                          l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11+ l12 + l13,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    for l10 in x:
+                                        for l11 in x:
+                                            for l12 in x:
+                                                for l13 in x:
+                                                    for l14 in x:
+                                                        n = l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13+ l14
+                                                        cekmd5(md5(n))
+                                                        print('cek :',
+                                                              l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13+ l14,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    for l10 in x:
+                                        for l11 in x:
+                                            for l12 in x:
+                                                for l13 in x:
+                                                    for l14 in x:
+                                                        for l15 in x:
+                                                            n = l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13 + l14+ l15
+                                                            cekmd5(md5(n))
+                                                            print('cek :',
+                                                                  l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13 + l14+ l15,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    for l10 in x:
+                                        for l11 in x:
+                                            for l12 in x:
+                                                for l13 in x:
+                                                    for l14 in x:
+                                                        for l15 in x:
+                                                            for l16 in x:
+                                                                n = l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13 + l14 + l15+ l16
+                                                                cekmd5(md5(n))
+                                                                print('cek :',
+                                                                      l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13 + l14 + l15+ l16,end="\r")
+for l1 in x:
+    for l2 in x:
+        for l3 in x:
+            for l4 in x:
+                for l5 in x:
+                    for l6 in x:
+                        for l7 in x:
+                            for l8 in x:
+                                for l9 in x:
+                                    for l10 in x:
+                                        for l11 in x:
+                                            for l12 in x:
+                                                for l13 in x:
+                                                    for l14 in x:
+                                                        for l15 in x:
+                                                            for l16 in x:
+                                                                for l17 in x:
+                                                                    n = l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13 + l14 + l15 + l16+ l17
+                                                                    cekmd5(md5(n))
+                                                                    print('cek :',
+                                                                          l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13 + l14 + l15 + l16+ l17,end="\r")
+
+
+
+
+print('Not Found!')
+
